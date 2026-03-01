2a9e2d5af895573145779e33fe71ae8a4e2b9a36
yang.yang <yang.yang@databricks.com>
2026-02-28 23:01:39 -0800

    clean up folder for PR to main

--- config.py
+++
@@ -1,9 +0,0 @@
-import warnings
-from src.multi_agent.core.config import *
-
-warnings.warn(
-    "The root config.py is deprecated and will be removed in a future version. "
-    "Please import from src.multi_agent.core.config instead.",
-    DeprecationWarning,
-    stacklevel=2
-)
